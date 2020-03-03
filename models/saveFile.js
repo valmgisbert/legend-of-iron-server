c4ca05888b36e7a02d92d376edfdd4a14e000e8c
Val GZ <valmgisbert@gmail.com>
2020-03-03 19:10:22 +0100

    first commit

---
+++ models/saveFile.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const saveFileSchema = new Schema({
+  user: {type: Schema.Types.ObjectId, ref: "User"},
+  gameState: [{choiceId}]
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  },
+});
+
+const SaveFile = mongoose.model('SaveFile', saveFileSchema);
+
+module.exports = SaveFile;
---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+	studentName: {type: String, required: false},
+	cohort: {type: String, required: false}, 
+	saveSlots: [{type: Schema.Types.ObjectId, ref: "SaveFile"}]
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
